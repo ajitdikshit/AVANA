fe35f7ed286b89eaf5e27e3663a58e996ea959eb
ajitdikshit <dikshitajit32@gmail.com>
2026-03-21 20:58:01 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'avana-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/garden.html',
+  '/community.html',
+  '/wiki.html',
+  '/diseases.html',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install the Service Worker and Cache Files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve Cached Files when Offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return the cached version if found, otherwise fetch from the network
+        return response || fetch(event.request);
+      })
+  );
+});
